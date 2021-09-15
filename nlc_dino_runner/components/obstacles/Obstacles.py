fddef8a7013452a0359704d5cca9af4d3bcd1e47
DxxgxPxrxz <diegoguitarra64@gmail.com>
2021-09-15 19:56:02 -0400

    Clase 5: Menu

--- nlc_dino_runner/components/obstacles/Obstacles.py
+++ nlc_dino_runner/components/obstacles/Obstacles.py
@@ -1,14 +1,16 @@
-from pygame.sprite import Sprite
-from nlc_dino_runner.utils.constants import SCREEN_WIDTH
+#EN EL FILE OBSTACLES VIVE LA CLASS PADRE OBSTACLES, QUE A SU VEZ ES CLASE HIJA DE PYGAME.SPRITE "Sprite"
+from pygame.sprite import Sprite #Importando una clase de mayor jerarquia(Herencia)
+from nlc_dino_runner.utils.constants import SCREEN_WIDTH #Importando constantes
 #Clase padre
 
 
 
 class Obstacles(Sprite):
+
     def __init__(self, image, obstacle_type):
         self.image = image
         self.obstacle_type = obstacle_type
-        self.rect = self.image[self.obstacle_type].get_rect() #Retorna una tuple
+        self.rect = self.image[self.obstacle_type].get_rect() #Retorna una tupla
         self.rect.x = SCREEN_WIDTH #1100
 
     def update(self, game_speed, obstacles_list):
@@ -16,11 +18,8 @@ class Obstacles(Sprite):
         if self.rect.x < -self.rect.width:
             obstacles_list.pop()
 
-
-
     def draw(self, screen):
         screen.blit(self.image[self.obstacle_type], self.rect)
-
 
 
 
--- nlc_dino_runner/main.py
+++ nlc_dino_runner/main.py
@@ -1,7 +1,8 @@
-from nlc_dino_runner.components.game import Game
+#EL ARCHIVO MAIN SE VA A ENCARGAR DE CORRER EL JUEGO
 
-if __name__ == "__main__":
-    game = Game()
-    game.run()
 
-#Clase 3: Dino
+from nlc_dino_runner.components.game import Game #Importando una class desde un Python File proveniente de la carpeta COMPONENTS (en este caso la class llamada "Game")
+
+if __name__ == "__main__": #El __name__ se convierte en __main__ mientras el archivo principal es ejecutado
+    game = Game() #Definiendo la var game como objeto, con la class importada Game, para correr el juego
+    game.execute() 
